9b8fb415c1d7150abaf8e2e77b9dfd23728d14e7
LaBriquez <72671532+LaBriquez@users.noreply.github.com>
2025-04-09 22:06:11 +0200

    Update shader.glsl

--- shader.glsl
+++ shader.glsl
@@ -9,8 +9,8 @@ vec4 sphere = vec4(-1.0, -2.0, 5.0, 2.0);
 
 void main() {
     vec2 uv = (gl_FragCoord.xy * 2.0 - resolution.xy) / resolution.x;
-
     vec3 N = normalize(vec3(uv.x, uv.y, 1.0));
+
     vec3 A = dot(sphere.xyz, N) * N;
     vec3 dir = A - sphere.xyz;
     float len = dot(dir, dir) / (sphere.w * sphere.w);
